8bd5ec431fd4e25bc42625cf319c63b3897bf9f1
DoodleBob <riley.foust18@gmail.com>
2022-01-22 15:39:50 -0500

    added conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from './components/Header';
 import About from "./components/About";
 import Portfolio from './components/Portfolio';
 import ContactForm from "./components/Contact";
 import Resume from './components/Resume';
 import Footer from "./components/Footer";
+import { capitalizeFirstLetter } from "./utils/helpers";
 
 function App() {
+  const [categories] = useState(['about', 'portfolio', 'contact', 'resume']);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory);
+  }, [currentCategory]);
+
   return (
-    <div>
-      <Header></Header>
-      <About></About>
-      <Portfolio></Portfolio>
-      <ContactForm></ContactForm>
-      <Resume></Resume>
+    <body>
+      <Header
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Header>
+      <main>
+        {currentCategory === 'about' && 
+          <About></About>
+        }
+        {currentCategory === 'portfolio' && 
+          <Portfolio></Portfolio>
+        }
+        {currentCategory === 'contact' && 
+          <ContactForm></ContactForm>
+        }
+        {currentCategory === 'resume' && 
+          <Resume></Resume>
+        }
+      </main>
       <Footer></Footer>
-    </div>
+    </body>
   )
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,12 @@
 import React, {useState, useEffect} from "react";
 import Nav from '../Nav'
-import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Header() {
-  const [categories] = useState(['about', 'portfolio', 'contact', 'resume']);
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory);
-  }, [currentCategory]);
-
+function Header(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
   return (
     <header className="flex-row px-1">
       <h2>
