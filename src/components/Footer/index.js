22cd848881ed23d72f1cf88793887f433246905c
DoodleBob <riley.foust18@gmail.com>
2022-01-22 14:54:15 -0500

    added footer and logos

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,21 @@
 import React, {useState, useEffect} from "react";
-import linkedIn from '../../assets/images/linkedin-logo.png'
+import linkedIn from '../../assets/images/linkedin-logo.png';
+import github from '../../assets/images/github-icon.png';
+import stackOverflow from '../../assets/images/stackoverflow-icon.png'
 
 function Footer() {
   return (
     <footer className="flex-row">
       <div className="footer-icons">
-        <a href="https://www.linkedin.com/in/rileyfoust/" className="icon">
+        <a href="https://www.linkedin.com/in/rileyfoust/" className="mx-2 icon">
           <img src={linkedIn}/>
         </a>
-        <p className="mx-1 icon">Foot</p>
-        <p className="mx-1 icon">Foot</p>
+        <a href="https://github.com/riley-foust18" className="mx-2 icon">
+          <img src={github}/>
+        </a>
+        <a href="https://stackoverflow.com/users/16518340/riley-foust18" className="mx-2 icon">
+          <img src={stackOverflow}/>
+        </a>
       </div>
     </footer>
   );
