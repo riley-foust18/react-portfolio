5105556c738026d2b031e0e88f7f4402474c7d4d
DoodleBob <riley.foust18@gmail.com>
2022-01-23 17:53:58 -0500

    added resume section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,8 +5,8 @@ function About() {
   return (
     <div className="about-me">
       <h2 className="mx-2">About Me</h2>
-      <div className="flex-row">
-        <img src={photo} className="mx-2" style={{width: '40%'}} />
+      <div className="flex-row mx-2">
+        <img src={photo} style={{width: '40%'}} />
         <p className="mx-2">
           My name is Riley and I am a full time fabrication engineer and a new web devleoper. 
           I'm currently enrolled in a coding bootcamp, with the end goal of making a career change.
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,8 +2,33 @@ import React, {useEffect} from "react";
 
 function Resume() {
   return (
-    <div>
-      <h2>Resume</h2>
+    <div className="resume">
+      <h2 className="mx-2">Resume</h2>
+      <p className="mx-2">Download my resume <a style={{'text-decoration': 'underline'}} href="https://docs.google.com/document/d/14Vj0TTzaR7pWEDq9bV855F3mIz8fPeMTCQ4-kDu3dXc/edit?usp=sharing">here</a></p>
+      <div>
+        <h3 className="mx-2">Front-end Skills</h3>
+        <ul className="mx-2">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>JQuery</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+        </ul>
+
+        <h3 className="mx-2">Back-end Skills</h3>
+        <ul className="mx-2">
+          <li>APIs</li>
+          <li>REST</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL</li>
+          <li>Sequlize</li>
+          <li>MongoDB</li>
+          <li>Mongoose</li>
+          <li>GraphQL</li>
+        </ul>
+      </div>
     </div>
   );
 }
